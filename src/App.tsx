b5acbdd0f4a4bf28a4260f11bb4f53def6f556e5
Enver <enver.erfanovich@gmail.com>
2026-06-08 13:18:04 +0300

    init: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import { Wallet } from "lucide-react";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./components/ui/dropdown-menu";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
 
 function App() {
   return (
     <div>
-      <h2 className="text-3xl text-red-500 font-bold underline">ClickInvite</h2>
-      <Wallet className="size-10" />
+      <Input placeholder="Enter your email" />
+      <Button>Click me</Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button>Click me</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Click me</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Click me</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Click me</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>Click me</DialogDescription>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
