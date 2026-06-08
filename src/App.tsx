9910cd2095bb61599cd57df3f5fee5b497f21d1d
Enver <enver.erfanovich@gmail.com>
2026-06-08 12:30:50 +0300

    init: lucide icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Wallet } from "lucide-react";
+
 function App() {
   return (
-    <h2 className="text-3xl text-red-500 font-bold underline">ClickInvite</h2>
+    <div>
+      <h2 className="text-3xl text-red-500 font-bold underline">ClickInvite</h2>
+      <Wallet className="size-10" />
+    </div>
   );
 }
 
